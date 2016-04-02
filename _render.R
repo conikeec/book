ffe845c64fff23c26f457d8f72e341d285bd8b1c
qinwf <mail@qinwenfeng.com>
2016-04-02 13:02:26 +0800

    Add: type table

--- _render.R
+++ _render.R
@@ -20,9 +20,11 @@ if (length(formats) == 0) formats = c(
 )
 # render the book to all formats unless they are specified via command-line args
 for (fmt in formats) {
+  Sys.setenv(BOOKDOWNOUT = fmt)
   cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = %s)", fmt, quiet)
   res = bookdown:::Rscript(c('-e', shQuote(cmd)))
   if (res != 0) stop('Failed to compile the book to ', fmt)
   if (fmt == 'bookdown::epub_book') bookdown::kindlegen()
+  Sys.unsetenv("BOOKDOWNOUT")
 }
 
